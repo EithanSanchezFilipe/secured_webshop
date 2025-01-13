4bf791774c0d12530e2dce4a157f147b1748fc1c
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-01-13 15:08:18 +0100

    chore : add html file

--- app/server.js
+++ app/server.js
@@ -8,6 +8,7 @@ const userRoute = require('./routes/User');
 const app = express();
 
 app.use('/user', userRoute);
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 const certifPath = path.join(__dirname, '/keys/certificate.crt');
 const privKeyPath = path.join(__dirname, '/keys/private.key');
@@ -15,12 +16,14 @@ const privKeyPath = path.join(__dirname, '/keys/private.key');
 const CERTIFICAT = fs.readFileSync(certifPath, 'utf8');
 const PRIVATE_KEY = fs.readFileSync(privKeyPath, 'utf8');
 
-
-
 const server = https.createServer({
     key: PRIVATE_KEY,
     cert: CERTIFICAT
 }, app);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+  });
 
 // Démarrage du serveur
 server.listen(8080, () => {
