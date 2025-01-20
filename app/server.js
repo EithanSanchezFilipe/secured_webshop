ec9c29623590f736bc62232829b4c3a1e76225c4
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-01-20 13:27:25 +0100

    feat: main page

--- app/server.js
+++ app/server.js
@@ -4,11 +4,9 @@ const https = require('https');
 const express = require('express');
 const userRoute = require('./routes/User');
 
-
 const app = express();
 
 app.use('/user', userRoute);
-app.use(express.static(path.join(__dirname, '../frontend')));
 
 const certifPath = path.join(__dirname, '/keys/certificate.crt');
 const privKeyPath = path.join(__dirname, '/keys/private.key');
@@ -16,16 +14,21 @@ const privKeyPath = path.join(__dirname, '/keys/private.key');
 const CERTIFICAT = fs.readFileSync(certifPath, 'utf8');
 const PRIVATE_KEY = fs.readFileSync(privKeyPath, 'utf8');
 
-const server = https.createServer({
+app.use(express.static(path.join('./public', 'css')));
+
+const server = https.createServer(
+  {
     key: PRIVATE_KEY,
-    cert: CERTIFICAT
-}, app);
+    cert: CERTIFICAT,
+  },
+  app
+);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
 
 // Démarrage du serveur
 server.listen(8080, () => {
-    console.log('Server running on port 8080');
+  console.log('Server running on port https://localhost:8080');
 });
