94acd8ef0b86b4a328f82a52ab9fbbd7fd5ba9fd
EithanSanchezFilipe <150446486+EithanSanchezFilipe@users.noreply.github.com>
2025-02-05 00:08:55 +0100

    fix : cookie expires

--- app/controllers/UserController.mjs
+++ app/controllers/UserController.mjs
@@ -84,7 +84,7 @@ async function login(req, res) {
         httpOnly: true,
         sameSite: 'strict',
         secure: process.env.NODE_ENV === 'production',
-        maxAge: 1 * 60 * 60 * 1000,
+        maxAge: 24 * 60 * 60 * 1000,
       });
       return res.status(200).json({ message: 'Connexion reussie', token });
     });
